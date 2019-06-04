20b510bcca1d08de010bdfb58d728150e625c5ee
KosukeFujii <konkon.317@gmail.com>
2019-06-04 17:34:08 +0900

    added scripts for NetworkServer and NetworkClient class testing

---
+++ NetworkingSandBox/Assets/Source/Client.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Networking;
+using System.Text;
+
+public class Client : MonoBehaviour
+{
+
+    const short MessageTypeTest = MsgType.Highest + 1;
+
+    // Update is called once per frame
+
+    class Message : MessageBase
+    {
+        public byte[] message;
+    }
+
+    Message _m = new Message();
+
+    StringBuilder _builder = new StringBuilder();
+
+    byte[] _message;
+    NetworkClient _client = new NetworkClient();
+    bool _clientConnected = false;
+
+    // Use this for initialization
+    void Start ()
+    {
+        _message = new byte[16];
+        for (byte i = 0; i < 16; i++)
+        {
+            _message[i] = (byte)(15 + i);
+        }
+
+        _client.RegisterHandler(MessageTypeTest, networkMessage =>
+        {
+            var reader = networkMessage.reader;
+            int length = reader.ReadByte() + (256 * reader.ReadByte());
+            var mes = networkMessage.reader.ReadBytes(reader.Length - (int)reader.Position);
+
+            _builder.Length = 0;
+            _builder.Append("client received : ");
+            _builder.Append(length).Append("  : ");
+            foreach (var b in mes)
+            {
+                _builder.Append(b.ToString()).Append(",");
+            }
+            Debug.Log(_builder);
+
+            _message = mes;
+
+        });
+
+        _client.RegisterHandler(MsgType.Connect, _ => { _clientConnected = true; });
+        _client.RegisterHandler(MsgType.Disconnect, _ => { _clientConnected = false; });
+        
+        _client.Connect("127.0.0.1", 7000);
+    }
+
+    void Update()
+    {
+        if (_clientConnected)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _m.message = _message;
+                _client.Send(MessageTypeTest, _m);
+            }
+        }
+
+    }
+}
---
+++ NetworkingSandBox/Assets/Source/Server.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Networking;
+
+using System.Text;
+
+public class Server: MonoBehaviour
+{
+
+    const short MessageTypeTest= MsgType.Highest+1;
+	
+	// Update is called once per frame
+
+    class Message:MessageBase
+    {
+        public byte[] message;
+    }
+
+    Message _m = new Message();
+
+    StringBuilder _builder=new StringBuilder();
+
+    byte[] _message;
+ 
+    private void Awake()
+    {
+        NetworkServer.RegisterHandler(MessageTypeTest, networkmessage =>
+        {
+            var reader = networkmessage.reader;
+            int length = reader.ReadByte() + (256 * reader.ReadByte());
+            var mes = reader.ReadBytes(reader.Length - (int)reader.Position);
+
+            _builder.Length = 0;
+            _builder.Append("Host received : ");
+            _builder.Append(length).Append("  : ");
+            foreach (var b in mes)
+            {
+                _builder.Append(b.ToString()).Append(",");
+            }
+            Debug.Log(_builder);
+
+            for (int i = 0; i < mes.Length; i++)
+            {
+                mes[i]++;
+            }
+
+            _m.message = mes;
+            networkmessage.conn.Send(MessageTypeTest, _m);
+        }
+        );
+
+        NetworkServer.Listen("127.0.0.1", 7000);
+
+        
+    }
+   
+}
